210ca5fb2bd1d9ff7d4246d32eab640384060c46
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2021-09-24 23:46:02 +0530

    Bug fix: used less random key for deployment

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def ssl_disabled_urlopen(endpoint):
     return urllib.request.urlopen(endpoint, context=context)
 
 app = flask.Flask(__name__)
-app.secret_key = uuid4().bytes
+app.secret_key = "Something dangerous..."
 client = amadeus.Client(http = ssl_disabled_urlopen)
 
 @app.template_filter()
